fde29b4bdc5b4f872f2603a912a42ee195526fc6
George Legeza <George Legeza>
2016-12-29 15:03:09 -0500

    Add project for unit testing

---
+++ StarformNET.UnitTests/UnitConversionsTest.cs
@@ -0,0 +1,291 @@
+namespace DLS.StarformNET.UnitTests
+{
+
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using StarformNet;
+
+    public class UnitConversionsTest
+    {
+        public static double DELTA = 0.01;
+
+        [TestClass]
+        public class KelvinToFahrenheitTests
+        {
+
+            [TestMethod]
+            public void ConvertAbsoluteZero()
+            {
+                double resultZero = UnitConversions.KelvinToFahrenheit(0.0);
+                Assert.AreEqual(resultZero, -459.67, DELTA, "Absolute zero converted incorrectly.");
+            }
+
+            [TestMethod]
+            public void ConvertPositiveKelvin()
+            {
+                var tempK = new double[]
+                {
+                    20,
+                    78,
+                    120,
+                    1000,
+                    1234,
+                    36000
+                };
+
+                var expectedTemp = new double[]
+                {
+                    -423.67,   // 20 K
+                    -319.27,   // 78 K
+                    -243.67,   // 120 K
+                    1340.33,   // 1000 K
+                    1761.53,   // 1234 K
+                    64340.33   // 36000 K
+                };
+
+                for (var i = 0; i < tempK.Length; i++)
+                {
+                    var result = UnitConversions.KelvinToFahrenheit(tempK[i]);
+                    Assert.AreEqual(result, expectedTemp[i], DELTA,
+                        String.Format("Incorrect result converting {0} K. Expected {1}, received {2}",
+                        tempK[i], expectedTemp[i], result));
+                }
+            }
+        }
+
+        [TestClass]
+        public class MMHGToMillibarsTests
+        {
+            [TestMethod]
+            public void ConvertZeroPressure()
+            {
+                var result = UnitConversions.MMHGToMillibars(0);
+                Assert.AreEqual(result, 0.0, DELTA, "Incorrect conversion for zero mmHg");
+            }
+
+            [TestMethod]
+            public void ConvertPositivePressureLessThanOne()
+            {
+                var presMmHg = new double[]
+                {
+                    0.20,
+                    0.78,
+                    0.120,
+                    0.1,
+                    0.1234,
+                    0.0005,
+                    0.002
+                };
+
+                var expectedMb = new double[]
+                {
+                    0.266645,     // 0.2 mmHg
+                    1.039915,     // 0.78 mmHg
+                    0.1599869,    // 0.120 mmHg
+                    0.133322,     // 0.1 mmHg
+                    0.164519826,  // 0.1234 mmHg
+                    0.0066661194, // 0.0005 mmHg
+                    0.0026664477  // 0.002 mmHg
+                };
+
+                for (var i = 0; i < presMmHg.Length; i++)
+                {
+                    var result = UnitConversions.MMHGToMillibars(presMmHg[i]);
+                    Assert.AreEqual(result, expectedMb[i], DELTA,
+                        String.Format("Incorrect result converting {0} mmHg. Expected {1}, received {2}",
+                        presMmHg[i], expectedMb[i], result));
+                }
+            }
+
+            [TestMethod]
+            public void ConvertPositivePressureWholeNumbers()
+            {
+                var presMmHg = new double[]
+                {
+                    20,
+                    78,
+                    120,
+                    1000,
+                    1234,
+                    36000,
+                    120000
+                };
+
+                var expectedMb = new double[]
+                {
+                    26.6645,    // 20 mmHg
+                    103.991,    // 78 mmHg
+                    159.987,    // 120 mmHg
+                    1333.22,    // 1000 mmHg
+                    1645.198,   // 1234 mmHg
+                    47996.059,  // 36000 mmHg
+                    159986.865  // 120000 mmHg
+                };
+
+                for (var i = 0; i < presMmHg.Length; i++)
+                {
+                    var result = UnitConversions.MMHGToMillibars(presMmHg[i]);
+                    Assert.AreEqual(result, expectedMb[i], DELTA,
+                        String.Format("Incorrect result converting {0} mmHg. Expected {1}, received {2}",
+                        presMmHg[i], expectedMb[i], result));
+                }
+            }
+        }
+
+        [TestClass]
+        public class MillibarsToAtmTests
+        {
+            [TestMethod]
+            public void ConvertZeroPressure()
+            {
+                var result = UnitConversions.MillibarsToAtm(0);
+                Assert.AreEqual(result, 0.0, DELTA, "Incorrect conversion for zero mmHg");
+            }
+
+            [TestMethod]
+            public void ConvertEarthSurfacePressure()
+            {
+                var result = UnitConversions.MillibarsToAtm(GlobalConstants.EARTH_SURF_PRES_IN_MILLIBARS);
+                Assert.AreEqual(result, 1.0, DELTA, "Incorrect conversion for Earth surface pressure.");
+            }
+
+            [TestMethod]
+            public void ConvertPositivePressureLessThanOne()
+            {
+                var presMb = new double[]
+                {
+                    0.20,
+                    0.78,
+                    0.120,
+                    0.1,
+                    0.1234,
+                    0.0005,
+                    0.002,
+                    0.9
+                };
+
+                var expectedAtm = new double[]
+                {
+                    0.000197385,     // 0.2 mb
+                    0.0007698001,    // 0.78 mb
+                    0.0001184308,    // 0.120 mb
+                    9.8692e-5,       // 0.1 mb
+                    0.000121786331,  // 0.1234 mb
+                    4.9346163e-7,    // 0.0005 mb
+                    1.973847e-6,     // 0.002 mb
+                    0.000888231      // 0.9 mb
+                };
+
+                for (var i = 0; i < presMb.Length; i++)
+                {
+                    var result = UnitConversions.MillibarsToAtm(presMb[i]);
+                    Assert.AreEqual(result, expectedAtm[i], DELTA,
+                        String.Format("Incorrect result converting {0} mb. Expected {1}atm, received {2}atm",
+                        presMb[i], expectedAtm[i], result));
+                }
+            }
+
+            [TestMethod]
+            public void ConvertPositivePressureWholeNumbers()
+            {
+                var presMb = new double[]
+                {
+                    20,
+                    78,
+                    120,
+                    1000,
+                    1234,
+                    36000,
+                    120000
+                };
+
+                var expectedAtm = new double[]
+                {
+                    0.0198,    // 20 mb
+                    0.0770,    // 78 mb
+                    0.1185,    // 120 mb
+                    0.9870,    // 1000 mb
+                    1.2179,    // 1234 mb
+                    35.5293,   // 36000 mb
+                    118.4308   // 120000 mb
+                };
+
+                for (var i = 0; i < presMb.Length; i++)
+                {
+                    var result = UnitConversions.MillibarsToAtm(presMb[i]);
+                    Assert.AreEqual(result, expectedAtm[i], DELTA,
+                        String.Format("Incorrect result converting {0} mb. Expected {1}atm, received {2}atm",
+                        presMb[i], expectedAtm[i], result));
+                }
+            }
+        }
+
+        [TestClass]
+        public class PPMToMillibars
+        {
+            private static double[] ATMOS = new double[] { 1, 1.5, 0.5, 4, 0.1 };
+            private static double[] PPM_VALUES = new double[]
+            {
+                20,
+                78,
+                120,
+                1,
+                1234,
+                55000,
+                100000,
+                25000
+            };
+
+            [TestMethod]
+            public void ConvertZeroPPM()
+            {
+                for (var i = 0; i < ATMOS.Length; i++)
+                {
+                    var result = UnitConversions.PPMToMillibars(0, ATMOS[i]);
+                    Assert.AreEqual(0.0, result, DELTA);
+                }
+            }
+
+            [TestMethod]
+            public void Convert1MPPM()
+            {
+                for (var i = 0; i < ATMOS.Length; i++)
+                {
+                    var result = UnitConversions.PPMToMillibars(1000000, ATMOS[i]);
+                    Assert.AreEqual(result, GlobalConstants.EARTH_SURF_PRES_IN_MILLIBARS * ATMOS[i], DELTA);
+                }
+            }
+
+            [TestMethod]
+            public void ConvertPositivePPMWholeNumbers1Atm()
+            {
+                for (var i = 0; i < PPM_VALUES.Length; i++)
+                {
+                    var fract = PPM_VALUES[i] / 1000000;
+                    var expected = GlobalConstants.EARTH_SURF_PRES_IN_MILLIBARS * fract;
+                    var result = UnitConversions.PPMToMillibars(PPM_VALUES[i]);
+                    Assert.AreEqual(expected, result, DELTA,
+                        String.Format("expected: {0} ppm @ {1} atm -> {2} mb, result: {3}",
+                        PPM_VALUES[i], 1.0, expected, result));
+                }
+            }
+
+            [TestMethod]
+            public void ConvertPositivePPMWholeNumbersAboveBelow1Atm()
+            {
+                for (var i = 0; i < PPM_VALUES.Length; i++)
+                {
+                    var fract = PPM_VALUES[i] / 1000000;
+                    for (var k = 0; k < ATMOS.Length; k++)
+                    {
+                        var expected = GlobalConstants.EARTH_SURF_PRES_IN_MILLIBARS * ATMOS[k] * fract;
+                        var result = UnitConversions.PPMToMillibars(PPM_VALUES[i], ATMOS[k]);
+                        Assert.AreEqual(expected, result, DELTA,
+                            String.Format("expected: {0} ppm @ {1} atm -> {2} mb, result: {3}",
+                            PPM_VALUES[i], ATMOS[k], expected, result));
+                    }
+                }
+            }
+        }
+    }
+}
