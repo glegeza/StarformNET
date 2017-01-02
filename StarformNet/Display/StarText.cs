2c46cf52521374920dba77432c49256c287f2b39
George Legeza <glegeza@gmail.com>
2017-01-02 14:11:59 -0500

    Add comments

--- StarformNet/Display/StarText.cs
+++ StarformNet/Display/StarText.cs
@@ -6,6 +6,10 @@
 
     public static class StarText
     {
+        /// <summary>
+        /// Returns a simple, multi-line string containing basic
+        /// information on a star using units relative to the Sun.
+        /// </summary>
         public static string GetFullStarTextRelative(Star star, bool showUnits=true)
         {
             StringBuilder sb = new StringBuilder();
@@ -19,6 +23,9 @@
             return sb.ToString();
         }
 
+        /// <summary>
+        /// Returns a string containing the age of a star relative to the Sun.
+        /// </summary>
         public static string GetAgeStringRel(Star star, bool showUnits=true)
         {
             var age = String.Format("{0:0.00}", star.Age / GlobalConstants.SUN_AGE_IN_YEARS);
@@ -26,6 +33,9 @@
             return age + units;
         }
 
+        /// <summary>
+        /// Returns a string containing the age of a star in years, displayed
+        /// using scientific notation.
         public static string GetAgeStringYearsSciN(Star star, bool showUnits=true)
         {
             var age = String.Format("{0:E2}", star.Age);
@@ -33,6 +43,10 @@
             return age + units;
         }
 
+        /// <summary>
+        /// Returns a string containing the luminosity of a star in Solar
+        /// Luminosity units.
+        /// </summary>
         public static string GetLuminosityRel(Star star, bool showUnits=true)
         {
             var lum = String.Format("{0:0.00}", star.Luminosity);
@@ -40,6 +54,10 @@
             return lum + units;
         }
 
+        /// <summary>
+        /// Returns a string containing the luminosity of a star in Solar
+        /// Mass units.
+        /// </summary>
         public static string GetMassRel(Star star, bool showUnits=true)
         {
             var mass = String.Format("{0:0.00}", star.Mass);
