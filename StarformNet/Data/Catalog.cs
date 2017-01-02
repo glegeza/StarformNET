d8fa705741fe33cbe494490a6d7862e360b44295
George Legeza <glegeza@gmail.com>
2017-01-02 14:12:58 -0500

    Remove unused class
    Leftover from porting. This class was used to store star catalogs, but I'll be replacing it with something else at some point and it isn't used anywhere right now.

--- StarformNet/Data/Catalog.cs
+++
@@ -1,9 +0,0 @@
-namespace DLS.StarformNET.Data
-{
-    public class Catalog
-    {
-        public int Count { get; set; }
-        public string Arg { get; set; }
-        public Star[] Stars { get; set; }
-    }
-}
