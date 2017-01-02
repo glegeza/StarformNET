a5d2e55ab7fc9b142b031e9d37026f2693316042
George Legeza <glegeza@gmail.com>
2017-01-02 14:07:01 -0500

    Add unit comments

--- StarformNet/Data/Star.cs
+++ StarformNet/Data/Star.cs
@@ -1,6 +1,5 @@
 namespace DLS.StarformNET.Data
 {
-    // TODO KnownPlanets should probably be a list? Or leaving it as a linked list might be fine too.
     // TODO units?
     // UGLY Not comfortable with binary systems just having a second mass value
 
@@ -8,10 +7,10 @@ namespace DLS.StarformNET.Data
     {
         public string Name { get; set; }
         public double Life { get; set; }
-        public double Age { get; set; }
+        public double Age { get; set; }                   // Years
         public double EcosphereRadius { get; set; }
-        public double Luminosity { get; set; }
-        public double Mass { get; set; }
+        public double Luminosity { get; set; }            // Sun = 1.0
+        public double Mass { get; set; }                  // Sun = 1.0
         public double BinaryMass { get; set; }
         public double SemiMajorAxis { get; set; }
         public double Eccentricity { get; set; }
