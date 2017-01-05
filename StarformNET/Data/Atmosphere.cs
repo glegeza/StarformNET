510829596be728d1b8b930381a3907cb644f411f
George Legeza <George Legeza>
2017-01-05 10:26:33 -0500

    Add atmosphere class for storing, uh, atmospheric data for a planet

---
+++ StarformNET/Data/Atmosphere.cs
@@ -0,0 +1,22 @@
+namespace DLS.StarformNET.Data
+{
+    
+    using System.Collections.Generic;
+
+    public class Atmosphere
+    {
+        public double SurfacePressure { get; set; }
+
+        public Breathability Breathability { get; set; }
+
+        public List<Gas> Composition { get; set; }
+
+        public List<Gas> PoisonousGases { get; set; }
+
+        public Atmosphere()
+        {
+            Composition = new List<Gas>();
+            PoisonousGases = new List<Gas>();
+        }
+    }
+}
