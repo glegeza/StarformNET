f1c96c958ef76a0984aa3c646005b9b7382e62a8
George Legeza <George Legeza>
2017-01-06 14:04:01 -0500

    Add ability to select planets by clicking on the system map

--- StarformNet/Display/SystemMap.cs
+++ StarformNet/Display/SystemMap.cs
@@ -1,5 +1,6 @@
 namespace DLS.StarformNET.Display
 {
+    using System;
     using System.Collections.Generic;
     using System.Drawing;
     using System.Windows.Forms;
@@ -11,6 +12,11 @@ namespace DLS.StarformNET.Display
         public int PlanetPadding { get; set; }
 
         private List<Sprite> _planetSprites = new List<Sprite>();
+        public int SelectedPlanetIndex = -1;
+        private Pen _selectionPen = new Pen(Color.White, 1);
+        private Pen _focusedSelectionPen = new Pen(Color.Red, 1);
+
+        public event EventHandler<EventArgs> PlanetClicked;
 
         public void SetNewSystem(List<Planet> planets)
         {
@@ -34,6 +40,68 @@ namespace DLS.StarformNET.Display
             Refresh();
         }
 
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            
+            switch (keyData)
+            {
+                case Keys.Left:
+                    SelectedPlanetIndex -= 1;
+                    if (SelectedPlanetIndex < 0)
+                    {
+                        SelectedPlanetIndex = _planetSprites.Count - 1;
+                    }
+                    PlanetClicked?.Invoke(this, EventArgs.Empty);
+                    return true;
+                case Keys.Right:
+                    SelectedPlanetIndex += 1;
+                    if (SelectedPlanetIndex >= _planetSprites.Count)
+                    {
+                        SelectedPlanetIndex = 0;
+                    }
+                    PlanetClicked?.Invoke(this, EventArgs.Empty);
+                    return true;
+            }
+            return (base.ProcessCmdKey(ref msg, keyData));
+        }
+
+        public void SelectPlanet(int index)
+        {
+            SelectedPlanetIndex = index;
+            Refresh();
+        }
+
+        protected override void OnClick(EventArgs e)
+        {
+            base.OnClick(e);
+            Focus();
+            var me = e as MouseEventArgs;
+            var localPos = me.Location;
+            for (var i = 0; i < _planetSprites.Count; i++)
+            {
+                var sprite = _planetSprites[i];
+                var rect = new Rectangle(sprite.DrawLocation, sprite.SourceRect.Size);
+                if (rect.Contains(localPos))
+                {
+                    SelectedPlanetIndex = i;
+                    PlanetClicked?.Invoke(this, EventArgs.Empty);
+                    return;
+                }
+            }
+        }
+
+        protected override void OnGotFocus(EventArgs e)
+        {
+            base.OnGotFocus(e);
+            Refresh();
+        }
+
+        protected override void OnLostFocus(EventArgs e)
+        {
+            base.OnLostFocus(e);
+            Refresh();
+        }
+
         protected override void OnPaintBackground(PaintEventArgs e)
         {
             base.OnPaintBackground(e);
@@ -41,6 +109,14 @@ namespace DLS.StarformNET.Display
             {
                 sprite.DrawSprite(e);
             }
+
+            if (SelectedPlanetIndex < 0 || SelectedPlanetIndex >= _planetSprites.Count)
+            {
+                return;
+            }
+            var selected = _planetSprites[SelectedPlanetIndex];
+            var pen = Focused ? _focusedSelectionPen : _selectionPen;
+            e.Graphics.DrawRectangle(pen, selected.DrawLocation.X - 1, selected.DrawLocation.Y - 1, selected.SourceRect.Width + 2, selected.SourceRect.Height + 2);
         }
     }
 }
