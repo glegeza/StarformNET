6afd95ef6be0288d8c79eb989459e62c0f183b94
George Legeza <George Legeza>
2017-01-09 13:20:07 -0500

    Add simple generator for creating random stars

---
+++ StarformNET/StarGenerator.cs
@@ -0,0 +1,35 @@
+namespace DLS.StarformNET
+{
+    using Data;
+    using System;
+
+    public static class StarGenerator
+    {
+        public static double MinSunAge = 1.0E9;
+        public static double MaxSunAge = 6.0E9;
+
+        public static Star GetDefaultStar()
+        {
+            var sun = new Star();
+
+            if (sun.Mass < 0.2 || sun.Mass > 1.5)
+            {
+                sun.Mass = Utilities.RandomNumber(0.7, 1.4);
+            }
+
+            if (sun.Luminosity == 0)
+            {
+                sun.Luminosity = Environment.Luminosity(sun.Mass);
+            }
+
+            sun.EcosphereRadius = Math.Sqrt(sun.Luminosity);
+            sun.Life = 1.0E10 * (sun.Mass / sun.Luminosity);
+
+            sun.Age = Utilities.RandomNumber(
+                MinSunAge,
+                sun.Life < MaxSunAge ? sun.Life : MaxSunAge);
+
+            return sun;
+        }
+    }
+}
